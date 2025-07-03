caecc28c081642466b4b320d56450535ef46b01d
shadanpk <sshadanpk@gmail.com>
2025-07-03 11:37:51 +0530

    updated UI

--- src/components/BlobBackground.tsx
+++ src/components/BlobBackground.tsx
@@ -8,6 +8,9 @@ const BlobBackground = () => {
       <div className={`${styles.blob} ${styles.red}`}></div>
       <div className={`${styles.blob} ${styles.yellow}`}></div>
       <div className={`${styles.blob} ${styles.green}`}></div>
+      <div className={`${styles.blob} ${styles.purple}`}></div>
+      <div className={`${styles.blob} ${styles.orange}`}></div>
+      <div className={`${styles.blob} ${styles.pink}`}></div>
     </div>
   );
 };
